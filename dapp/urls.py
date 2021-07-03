9365d3bf893b6ff85354ade4d311e707b7eee256
Trisha Shetty <trishashetty01@gmail.com>
2021-07-03 11:06:01 +0530

    Initial commit

---
+++ dapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from dapp import views
+
+urlpatterns = [
+    path("",views.home, name="home"),
+    path("home",views.home, name="home"),
+    path("pay",views.pay, name="pay"),
+    path("about",views.about, name="about"),
+    path("contact",views.contact, name="contact"),
+]
---
+++ dapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'donate.html')
+def pay(request):
+    return render(request, 'Pay.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
