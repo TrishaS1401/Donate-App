909e8a0d0e87fec0aa47b6c7dde0e12c5ba7dcee
Trisha Shetty <trishashetty01@gmail.com>
2021-07-03 17:31:52 +0530

    final commit

--- templates/pay.php
+++ templates/pay.php
@@ -1,9 +1,17 @@
 <script type="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+<form>
+    <input type="textbox" name="name" id="name" placeholder="Enter your name"/><br><br>
+    <input type="textbox" name="amt" id="amt" placeholder="Enter amount"/><br><br>
+    <input type="button" name="btn" id="btn" value="Pay now" onclick="paynow()"/><br><br>
+</form>
 <script>
+    function paynow(){
+    var name=jQuery('#name').val();
+    var amt=jQuery('#amt').val();
     var options = {
     "key": "rzp_test_W9KJOUi6tO2dX4", // Enter the Key ID generated from the Dashboard
-    "amount": "100", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "amount": amt*100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
     "currency": "INR",
     "name": "Acme Corp",
     "description": "Test Transaction",
@@ -11,17 +19,7 @@
     "handler": function (response){
         console.log(response);
     },
-    "prefill": {
-        "name": "Gaurav Kumar",
-        "email": "gaurav.kumar@example.com",
-        "contact": "9999999999"
-    },
-    "notes": {
-        "address": "Razorpay Corporate Office"
-    },
-    "theme": {
-        "color": "#3399cc"
-    }
+   
 };
 var rzp1 = new Razorpay(options);
 rzp1.on('payment.failed', function (response){
@@ -33,6 +31,7 @@ rzp1.on('payment.failed', function (response){
         alert(response.error.metadata.payment_id);
 });
     rzp1.open();
+    }
 </script>
 
 
