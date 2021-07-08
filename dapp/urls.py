86971c5e16787dc7be809162b2162b89edac0e6c
Trisha Shetty <trishashetty01@gmail.com>
2021-07-08 12:21:14 +0530

    final commit

--- dapp/urls.py
+++ dapp/urls.py
@@ -5,10 +5,6 @@ from dapp import views
 urlpatterns = [
     path("",views.home, name="home"),
     path("home",views.home, name="home"),
-    path("pay",views.pay, name="pay"),
     path("about",views.about, name="about"),
     path("contact",views.contact, name="contact"),
-    path("thankyou",views.thankyou, name="thankyou"),
-    path("mail",views.mail, name="mail"),
-    path("razor",views.razor, name="razor"),
 ]
--- dapp/views.py
+++ dapp/views.py
@@ -3,15 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'donate.html')
-def pay(request):
-    return render(request, 'Pay.html')
 def about(request):
     return render(request, 'about.html')
 def contact(request):
     return render(request, 'contact.html')
-def thankyou(request):
-    return render(request, 'thankyou.html')
-def mail(request):
-    return render(request, 'mail.php')
-def razor(request):
-    return render(request, 'pay.php')
